6b259f1787401b98a64e005c64fa924a97a7a23c
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 20:13:10 +0700

    Add files via upload

---
+++ PBO210118050Latihan10Logika/src/pbo210118050latihan10logika/PBO210118050Latihan10Logika.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118050latihan10logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan status dewasa menurut umur menggunakan logika
+ */
+public class PBO210118050Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+        
+        
+        
+    }
+    
+}
